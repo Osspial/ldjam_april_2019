9b5a3e6acc61bf5f49aa1361c714225a640477ad
Osspial <osspial@gmail.com>
2019-04-26 23:36:33 -0400

    second pass at movement and shooting

---
+++ Assets/Scripts/MoveWithRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MoveWithRotation : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Vector2 direction = Vector2.right;
+
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = direction.normalized * speed;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,23 +8,31 @@ public class Player : MonoBehaviour
     Rigidbody2D rigidbody;
 
     public float moveSpeedMultiplier = 10.0f;
+    public float shootKnockback = 1.0f;
     [Tooltip("max speed in relation to time since movement start")]
     public AnimationCurve maxSpeedCurve;
     public AnimationCurve timeSubtractAngleTurn;
     [Tooltip("gets capped at the length of the maxSpeed curve")]
     public float timeSinceMoveStart = 0.0f;
 
+    public MoveWithRotation bulletTemplate;
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    Vector2 mousePosition
+    {
+        get => Camera.main.ViewportToWorldPoint(Input.mousePosition / new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight));
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeedMultiplier;
-        Vector2 mousePos = Camera.main.ViewportToWorldPoint(Input.mousePosition);
-        Vector2 mouseDirection = (mousePos - transform.position.xy()).normalized;
+        Vector2 axisDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        Vector2 mouseDirection = (mousePosition - transform.position.xy()).normalized;
+        Vector2 moveDirection = axisDirection * moveSpeedMultiplier;
 
         if (moveDirection.magnitude > 0)
         {
@@ -37,6 +45,12 @@ public class Player : MonoBehaviour
         }
         rigidbody.velocity += moveDirection * Time.deltaTime;
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody.velocity -= mouseDirection * shootKnockback;
+            Shoot(mouseDirection);
+        }
+
         var maxSpeed = maxSpeedCurve.Evaluate(timeSinceMoveStart);
         if (moveDirection.magnitude > 0 && Vector2.Dot(rigidbody.velocity, moveDirection) > 0)
         {
@@ -45,5 +59,12 @@ public class Player : MonoBehaviour
                 rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
             }
         }
+    }
+
+    void Shoot(Vector2 direction)
+    {
+        MoveWithRotation bullet = Instantiate(bulletTemplate);
+        bullet.transform.position = transform.position;
+        bullet.direction = direction;
     }
 }
