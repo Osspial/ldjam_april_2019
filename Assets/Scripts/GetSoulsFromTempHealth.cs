69f645fe96ba759907f75f34ca8b13e658d3596f
Osspial <osspial@gmail.com>
2019-04-29 18:47:15 -0400

    Add things

--- Assets/Scripts/GetSoulsFromTempHealth.cs
+++ Assets/Scripts/GetSoulsFromTempHealth.cs
@@ -7,6 +7,7 @@ public class GetSoulsFromTempHealth : MonoBehaviour
 {
     public int souls = 0;
     public IntEvent onSoulsChanged;
+    public float rate = 2.0f;
 
     private Health health;
     // Start is called before the first frame update
@@ -15,13 +16,22 @@ public class GetSoulsFromTempHealth : MonoBehaviour
         health = GetComponent<Health>();
     }
 
+    float lastTake = 0;
     // Update is called once per frame
     void Update()
     {
-        if (AggroTracker.instance.aggros.Count == 0)
+        if (AggroTracker.instance.aggros.Count == 0 && Time.time - lastTake > rate && health.tempHealth.num > 0)
         {
-            souls += health.tempHealth.num / 2;
-            health.tempHealth.num = 0;
+            health.tempHealth.num -= 2;
+            if (health.tempHealth.num < 0)
+            {
+                health.tempHealth.num = 0;
+            }
+            else
+            {
+                souls += 1;
+            }
+            lastTake = Time.time;
         }
         onSoulsChanged.Invoke(souls);
     }
