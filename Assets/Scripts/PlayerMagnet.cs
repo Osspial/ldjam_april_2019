711744faabe1913f22e36bb4a196acd6e3a5e348
Osspial <osspial@gmail.com>
2019-04-29 03:07:27 -0400

    Begin adding soul collection

---
+++ Assets/Scripts/PlayerMagnet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMagnet : MonoBehaviour
+{
+    public Rigidbody2D move;
+    public float magnetSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerStay2D(Collider2D p)
+    {
+        if (p.GetComponent<Player>() is Player pl)
+        {
+            move.velocity += (pl.transform.position - move.transform.position).xy().normalized * magnetSpeed * Time.deltaTime;
+        }
+    }
+}
